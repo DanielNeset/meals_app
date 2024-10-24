eb7540886254a9bcce0290d443814f51dfd48289
Daniel Neset <danielneset@gmail.com>
2024-10-24 17:04:54 +0200

    Added images, lable and metadata to the meals item

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -36,6 +37,13 @@ class MealsScreen extends StatelessWidget {
             ),
           ],
         ),
+      );
+    }
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
       );
     }
 
